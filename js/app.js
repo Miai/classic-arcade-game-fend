bb8dd81fa1e7b976373907373fe5c84ee44d4c95
Mihai Moraru <tomihaimoraru@gmail.com>
2018-08-22 19:48:53 +0200

    Added enemies with different movement speeds

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ var numRows = 6,
     cWidth = 505;
     cHeight = 606;
 
+var speed = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(sprite, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = 404;
-    this.y = 145;
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.speed = speed (50,500);
 };
 
 // Update the enemy's position, required method for game
@@ -22,10 +27,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    let xArray = [0, 101, 202, 301, 401];
-    let yArray = [60, 145, 230];
-    console.log(this.x);
-
+    this.x += this.speed * dt;
+    if (this.x > 505) {
+        this.x = -100;
+        this.speed = speed (50,500);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -40,9 +46,9 @@ Enemy.prototype.render = function() {
 
 class Player {
     constructor(sprite, x, y) {
-        this.sprite = 'images/char-boy.png';
-        this.x = 202;
-        this.y = 400;
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
     }
 
     update(dt) {
@@ -50,13 +56,11 @@ class Player {
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        console.log(this.x, this.y);
     }
 
     handleInput(keyPress) {
         switch(keyPress) {
             case "left":
-                console.log ("left");
                 this.x -= 100;
                 break;
             case "up":
@@ -78,13 +82,18 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy()];
-var player = new Player();
+let allEnemies = [
+    new Enemy('images/enemy-bug.png', -100, 63),
+    new Enemy('images/enemy-bug.png', -50, 143),
+    new Enemy('images/enemy-bug.png', -20, 223)
+];
+var player = new Player('images/char-boy.png', 202, 400);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// Modified it to listen for "keydown" - like any gamer would want :))
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
